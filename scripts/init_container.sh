d035102704111df43f1d6172d6f1124e3d8f8cfa
Silvano Quarto <silvano.quarto@gmail.com>
2025-05-07 11:01:33 +0200

    Added initialization datasets for Docker Container

---
+++ scripts/init_container.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+echo "Initializing container..."
+
+# Environment
+export PYTHONPATH=/app
+
+# Script for checking dataset availabilty to be downloaded
+if [ ! -f "/app/data/datasets/cached/.initialized" ]; then
+    echo "Downloading datasets..."
+    python -m data.datasets.download_datasets --save-dir /app/data/datasets/cached
+    touch /app/data/datasets/cached/.initialized
+    echo "Datasets downloaded successfully!"
+else
+    echo "Datasets already downloaded"
+fi
+
+echo "Starting application..."
+exec "$@"
